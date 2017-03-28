cdce8a2e8689cc52ef2f753272ae221c23229d00
Your Name <s.nguyen11@utexas.edu>
2017-03-28 17:22:30 -0500

    html layout

---
+++ Moment.js
@@ -0,0 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyC0cRFojizk0dCWbXu94rFZZqb5CfPqf4U",
+    authDomain: "project-42d15.firebaseapp.com",
+    databaseURL: "https://project-42d15.firebaseio.com",
+    storageBucket: "project-42d15.appspot.com",
+    messagingSenderId: "1088789718722"
+  };
+  firebase.initializeApp(config);
+
+  
